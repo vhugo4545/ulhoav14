d286cd21e752a3d917f782d3b4ca386a5b5c56f0
vhugo4545 <43217997+vhugo4545@users.noreply.github.com>
2026-05-07 11:46:17 -0300

    Increase font size in resumo blocos textareas

--- js/preencher.js
+++ js/preencher.js
@@ -24,7 +24,7 @@ function esconderLoader() {
   dobrarFonteResumosBlocos();
 }
 
-function dobrarFonteResumosBlocos() {  document.querySelectorAll('textarea[id^="resumo-bloco-"]').forEach(textarea => {    textarea.style.fontSize = '1em';  });}
+function dobrarFonteResumosBlocos() {  document.querySelectorAll('textarea[id^="resumo-bloco-"]').forEach(textarea => {    textarea.style.fontSize = '1.5em';  });}
 
 
 // 📥 Extrai o ID da URL (?id=...)
